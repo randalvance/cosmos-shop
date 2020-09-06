13a6d8d0925a87bac1cf233a575be1186e23a8db
Randal Vance Cunanan <randal@randalvance.net>
2020-09-06 20:35:44 +0800

    Remove some dummy code

--- backend/src/greet.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(name) {
-    return `Hello there, ${name}!`;
-};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
-import greet from './greet';
 
 const app: express.Application = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    const name = req.query.name ?? 'Stranger';
-    const greeting = greet(name);
-    res.send(greeting);
+    res.send('Hello World');
 });
 
 app.listen(port, () => {
