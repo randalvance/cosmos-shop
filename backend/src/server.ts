ffeed3c7fc49d25d305720ed1f74feee31362164
Randal Vance Cunanan <randal@randalvance.net>
2020-09-06 21:14:29 +0800

    Test Commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,7 +4,7 @@ const app: express.Application = express();
 const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
